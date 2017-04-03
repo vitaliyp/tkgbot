26592c33880b948ec0efc965cfb770ef7ce17448
Vitaliy Popadiuk <vitaliypopadiuk@gmail.com>
2017-04-03 18:32:42 +0300

    Fixed formatting in forum.py.

--- forum.py
+++ forum.py
@@ -1,10 +1,10 @@
-import secret
-from bs4 import BeautifulSoup
-import requests
 import re
 import datetime
-from itertools import chain
 
+from bs4 import BeautifulSoup
+import requests
+
+import secret
 from models import NodeType
 
 NODE_LINK = 'https://www.tkg.org.ua/node/'
@@ -12,9 +12,11 @@ ROOT_LINK = 'https://www.tkg.org.ua'
 
 session = requests.Session()
 
+
 def _check_loginned(soup):
     profile_link = soup.find('a', href='/user')
     return bool(profile_link)
+
 
 def _login():
     data = {
@@ -23,11 +25,13 @@ def _login():
         'form_id': 'user_login',
     }
     session.post('https://www.tkg.org.ua/user',
-            data=data, allow_redirects=False)
+                 data=data, allow_redirects=False)
+
 
 def visit_topic(link):
     link = ''.join((ROOT_LINK, link))
     session.get(link)
+
 
 def get_id_from_link(link):
     m = re.match('/node/([\d]+)', link)
@@ -35,6 +39,7 @@ def get_id_from_link(link):
         return int(m.group(1))
     else:
         return None
+
 
 def get_updated_topics():
     topics = []
@@ -49,9 +54,9 @@ def get_updated_topics():
 
         topic_type_str = tr.find(class_='views-field-type').text.strip()
         topic['type'] = {'Тема в форумі': NodeType.TOPIC,
-                'Матеріал': NodeType.MATERIAL,
-                'Подія': NodeType.EVENT,
-                'Новина': NodeType.NEWS}.get(topic_type_str, None)
+                         'Матеріал': NodeType.MATERIAL,
+                         'Подія': NodeType.EVENT,
+                         'Новина': NodeType.NEWS}.get(topic_type_str, None)
         if not topic['type']:
             continue
 
@@ -66,14 +71,14 @@ def get_updated_topics():
         node_id = get_id_from_link(topic['link'])
         if not node_id:
             continue
-        topic['node_id'] = node_id 
+        topic['node_id'] = node_id
         section_a = tr.find('td', class_='views-field-og-group-ref').a
         if section_a:
             topic['section_name'] = section_a.text.strip()
             topic['section_link'] = section_a['href']
 
             section_node_id = get_id_from_link(topic['section_link'])
-            topic['section_node_id'] = section_node_id 
+            topic['section_node_id'] = section_node_id
         else:
             topic['section_name'] = None
             topic['section_link'] = None
@@ -84,9 +89,9 @@ def get_updated_topics():
         new_count = None
         total_count = None
         try:
-            if not count_strings or len(count_strings)>2:
+            if not count_strings or len(count_strings) > 2:
                 continue
-            if len(count_strings)==2:
+            if len(count_strings) == 2:
                 new_count = int(count_strings[1].split()[0])
             total_count = int(count_strings[0])
         except ValueError:
@@ -99,6 +104,7 @@ def get_updated_topics():
         topics.append(topic)
     return topics
 
+
 def _get_other_pages_links(soup):
     pager = soup.find('ul', class_='pager')
     if not pager:
@@ -106,12 +112,13 @@ def _get_other_pages_links(soup):
 
     links = []
     for li in pager.find_all('li', class_='pager-item'):
-       links.append(li.a['href']) 
+        links.append(li.a['href'])
 
     return links
 
+
 def _parse_datetime(datetime_string):
-    m =re.match('(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})([+-]\d{2}):(\d{2})', datetime_string)
+    m = re.match('(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})([+-]\d{2}):(\d{2})', datetime_string)
     if not m:
         return None
 
@@ -120,9 +127,11 @@ def _parse_datetime(datetime_string):
     tz = datetime.timezone(datetime.timedelta(hours=data[6], minutes=data[7]))
 
     return datetime.datetime(data[0], data[1], data[2], data[3], data[4], data[5], tzinfo=tz)
-    
+
+
 def _parse_comment_body(body):
     return '\n'.join(body.stripped_strings)
+
 
 def _get_new_comments_on_page(soup):
     marks = soup.find_all('mark', class_='new')
@@ -133,16 +142,16 @@ def _get_new_comments_on_page(soup):
 
         # Check if comment is a reply
         comment['is_reply'] = bool(comment_el.find_parent('div', class_='indented'))
-        
+
         header = comment_el.header
 
-        subj_a = header.find('a', class_='permalink') 
+        subj_a = header.find('a', class_='permalink')
         subject = subj_a.text
-        comment['subject'] = subj_a.text if subj_a.text!='.' else None
+        comment['subject'] = subj_a.text if subj_a.text != '.' else None
         comment['link'] = subj_a['href']
-        
+
         submitted = header.find('p', class_='submitted')
-        username_el = submitted.find(class_='username') 
+        username_el = submitted.find(class_='username')
         user_name = username_el.text
         comment['user_name'] = user_name
         if username_el.name == 'a':
@@ -169,14 +178,15 @@ def _get_new_comments_on_page(soup):
 
     return comments
 
+
 def get_new_comments_in_topic(link):
-        page = session.get(''.join((ROOT_LINK, link)))
-        soup = BeautifulSoup(page.text, 'html.parser')
-        comments = _get_new_comments_on_page(soup)
-        return comments
+    page = session.get(''.join((ROOT_LINK, link)))
+    soup = BeautifulSoup(page.text, 'html.parser')
+    comments = _get_new_comments_on_page(soup)
+    return comments
 
 
 _login()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     pass
