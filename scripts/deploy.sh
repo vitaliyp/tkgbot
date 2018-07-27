68cd6babc6f920b69b483924174a2f13b82b1c47
Vitaliy Popadiuk <vitaliypopadiuk@gmail.com>
2018-07-27 22:14:04 +0300

    Automatically deploy master branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,28 @@
-CONTAINER_NAME=tkgbot-staging
+CONTAINER_NAME="tkgbot-${TRAVIS_BRANCH}"
+IMAGE="$DOCKER_USERNAME/$DOCKER_REPO"
+
+source scripts/env-$TRAVIS_BRANCH.sh
+
+case $TRAVIS_BRANCH in
+develop)
+    volume_option=""
+    ;;
+master)
+    volume_option="--volume tkgbot-data:/tkgbot/data"
+    ;;
+*)
+    exit 1
+    ;;
+esac
 
 ssh travis@$DEPLOYMENT_HOST -i scripts/id_rsa << EOF
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker stop $CONTAINER_NAME
-    docker image rm $DOCKER_USERNAME/$DOCKER_REPO
-    docker pull $DOCKER_USERNAME/$DOCKER_REPO
-    docker run -d -e FORUM_LOGIN=$FORUM_LOGIN -e FORUM_PASSWORD=$FORUM_PASSWORD -e TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN --rm --name $CONTAINER_NAME $DOCKER_USERNAME/$DOCKER_REPO
+    docker image rm $IMAGE
+    docker pull $IMAGE:$TRAVIS_BRANCH
+    docker run -d -e "FORUM_LOGIN=$FORUM_LOGIN" \
+    -e "FORUM_PASSWORD=$FORUM_PASSWORD" \
+    -e "TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN" \
+    $volume_option \
+    --rm --name $CONTAINER_NAME $IMAGE:$TRAVIS_BRANCH
 EOF
