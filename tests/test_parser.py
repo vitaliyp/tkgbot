fc76be0b666266d4e597a870b92ad56b277c9d64
Vitalii Popadiuk <vitaliypopadiuk@gmail.com>
2018-09-18 17:52:01 +0300

    Add basic parser test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,5 @@
+import datetime
+
 from bs4 import BeautifulSoup
 
 from tkgbot import forum
@@ -9,7 +11,7 @@ comment_html = """<article class="comment first odd clearfix">
                                                             title="Переглянути профіль користувача."><img
                 src="https://www.tkg.org.ua/files/styles/75x75/public/pictures/picture-2610-1432375096.jpg?itok=wRa7OBDI"
                 alt="Зображення користувача РоСа." title="Зображення користувача РоСа."></a> </span> <a
-                href="/user/2610" title="Переглянути профіль користувача." class="username">РоСа</a> replied on
+                href="/user/2610" title="Переглянути профіль користувача." class="username">TestUser</a> replied on
             <time pubdate="" datetime="2018-08-08T16:07:00+03:00">Срд, 08/08/2018 - 16:07</time>
             <a href="/comment/225767#comment-225767" class="permalink" rel="bookmark">#</a></p>
     </header>
@@ -30,7 +32,7 @@ comment_html = """<article class="comment first odd clearfix">
     <div class="field field-name-comment-body field-type-text-long field-label-hidden">
         <div class="field-items">
             <div class="field-item even"><p></p>
-                <p>Идея груповой упаковки крутая.&nbsp;</p></div>
+                <p>Test Body.&nbsp;</p></div>
         </div>
     </div>
     <ul class="links inline">
@@ -49,4 +51,13 @@ def test_parse_simple_comment():
     print(comment)
 
     assert comment.link == '/comment/225767#comment-225767'
+    assert comment.reply_link == '/comment/reply/35676/225767'
     assert comment.anon == False
+    assert comment.subject == None
+    assert comment.user_name == 'TestUser'
+    assert comment.user_link == "/user/2610"
+    assert comment.date == datetime.datetime(2018, 8, 8, 16, 7,
+                                             tzinfo=datetime.timezone(datetime.timedelta(seconds=10800)))
+    assert comment.is_reply == False
+
+    assert str(comment.body) == 'Test Body.'
